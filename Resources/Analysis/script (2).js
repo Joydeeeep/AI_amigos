4dd9ade490d0ef52f49cc42460e661088b22034f
anumeha-0309 <111266352+anumeha-0309@users.noreply.github.com>
2024-07-19 16:52:23 +0000

    analysis

--- Resources/Analysis/script (2).js
+++ Resources/Analysis/script (2).js
@@ -1,7 +1,4 @@
-
 let lessonProgress = 0;
-let quizProgress = 0;
-let quizScore = 0;
 
 function completeLesson(amount) {
     lessonProgress += amount;
@@ -11,12 +8,4 @@ function completeLesson(amount) {
     document.getElementById('lessonProgressBar').style.width = lessonProgress + '%';
     document.getElementById('lessonProgressBar').textContent = lessonProgress + '%';
     document.getElementById('learningProgress').textContent = lessonProgress + '%';
-}
-
-function takeQuiz(score) {
-    quizScore = score;
-    quizProgress = (lessonProgress + quizScore) / 2;
-    document.getElementById('quizProgressBar').style.width = quizProgress + '%';
-    document.getElementById('quizProgressBar').textContent = quizProgress + '%';
-    document.getElementById('quizScore').textContent = quizScore + '%';
 }
